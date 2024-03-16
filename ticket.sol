bf4ab54753a0db4386fac1372c232a2bad71de42
Your Name <you@example.com>
2024-03-16 12:37:47 +0100

    add ticket functions

--- ticket.sol
+++ ticket.sol
@@ -1,17 +1,47 @@
-"""Juanka guapo"""
+pragma solidity ^0.8.0;
 
 
-struct ticket{
+contract Event{
+    struct Ticket{
 
-    # Event event
-    # User owner:
-    float price;
-    int ticketId;
-    string description;
-    bool forSale;
+        # Event event
+        # User owner:
+        float price;
+        int ticketId;
+        string description;
+        bool reSale;
 
+    }
+
+    mapping (address => ticket[]) public tickets;
+    mapping (address => ticket[]) public forSale;
+
+    event Bought(address buyer, uint amount);
+
+    error PaidAmountNotCorrect(float price);
+    error EventDoesNotAllowResale();
+
+    function buyTicket(Ticket ticket) external payable {
+        if (msg.value != ticket.price)
+            revert PaidAmountNotCorrect(ticket.price);
+        tickets[msg.sender].push(ticket);
+    }
+
+    function resaleTicket(Ticket ticket) external {
+        //TODO Optimize?
+        //bool found = False;
+        //sender_tickets = tickets[msg.sender]
+        
+        //for (uint i=0; i < sender_tickets.length; i++)
+        //    if (tickets[i] == ticket)
+        //        found = True;
+        //        break;
+        
+        if ticket.reSale == True
+            forSale[msg.sender].push(ticket);
+        else
+            revert EventDoesNotAllowResale();
+        
+        
+    }
 }
-
-uint256 public ticketId;
-
-mapping (unit256 => Ticket) public tickets;
