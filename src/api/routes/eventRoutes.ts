4ff960344510dd70ed8465748910964011eb50ab
Your Name <you@example.com>
2024-04-14 20:18:36 +0200

    add post

--- src/api/routes/eventRoutes.ts
+++ src/api/routes/eventRoutes.ts
@@ -18,18 +18,33 @@ router.get('/', async (req, res) => {
 router.get('/:id', (req, res) => {
     res.send(`User ${req.params.id} route!`);
 });
-
 //TODO: Test with frontend
-// router.post('/', async (req, res) => {
-//     try {
-//         const event = req.body as Event;
-//         const result = await collections.events?.insertOne(event);
-//         result
-//             ? res.status(201).send(`Successfully created a new game with id ${result.insertedId}`)
-//             : res.status(500).send("Failed to create a new game.");
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }); 
+router.post('/', async (req, res) => {
+    try {
+        const { title, description, startDate, endDate, location, image, uid} = req.body;
+
+        const new_event = new EventModel({
+            title,
+            description,
+            startDate,
+            endDate,
+            location,
+            image, 
+            uid
+          });
+        new_event.save()
+          .then(result => {
+            // Envía una respuesta de éxito al cliente
+            res.status(201).json({ message: 'Event created', user: result });
+          })
+          .catch(error => {
+            // Envía una respuesta de error al cliente
+            res.status(500).json({ error: 'Error when creating event', message: error.message });
+          });
+
+    } catch (error) {
+        console.error(error);
+    }
+}); 
 
 export default router;
--- src/api/routes/userRoutes.ts
+++ src/api/routes/userRoutes.ts
@@ -22,16 +22,30 @@ router.get('/:id', (req, res) => {
 });
 
 //TODO: Test with frontend
-// router.post('/', async (req, res) => {
-//     try {
-//         const user = req.body;
-//         const result = await collections.events?.insertOne(user);
-//         result
-//             ? res.status(201).send(`Successfully created a new game with id ${result.insertedId}`)
-//             : res.status(500).send("Failed to create a new game.");
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }); 
+router.post('/', async (req, res) => {
+    try {
+        const { username, email, password, roles, profilePictureUrl} = req.body;
+
+        const new_user = new UserModel({
+            username,
+            email,
+            password,
+            roles,
+            profilePictureUrl
+          });
+        new_user.save()
+          .then(result => {
+            // Envía una respuesta de éxito al cliente
+            res.status(201).json({ message: 'User created', user: result });
+          })
+          .catch(error => {
+            // Envía una respuesta de error al cliente
+            res.status(500).json({ error: 'Error when creating user', message: error.message });
+          });
+
+    } catch (error) {
+        console.error(error);
+    }
+}); 
 
 export default router;
