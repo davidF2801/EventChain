2699d386a689689d93dd7d6461ec896cf356fdd7
Your Name <you@example.com>
2024-04-20 17:43:16 +0200

    config

--- migrations/4_example_migration.js
+++ migrations/4_example_migration.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 var MyContract = artifacts.require('Event');
 module.exports = function (deployer) {
-  deployer.deploy(MyContract,100, process.env.TEST_ACCOUNT, 10);
+  deployer.deploy(MyContract,100, "TGDjYUzyDrPwBr19DzTNX4kgvanDv6ccMe", 10);
 };
