c4ab655ca4668e297029cc69954c06ebc45038cd
Your Name <you@example.com>
2024-04-21 18:34:29 +0200

    add create event

--- web_front/src/components/Events/NewEvent.js
+++ web_front/src/components/Events/NewEvent.js
@@ -1,34 +1,70 @@
 // NewEvent.js
-import React, { useState } from 'react';
-import './NewEvent.css';
+import React, { useState } from "react";
+import "./NewEvent.css";
 
 function NewEvent() {
-  const [eventName, setEventName] = useState('');
-  const [location, setLocation] = useState('');
-  const [date, setDate] = useState('');
-  const [eventType, setEventType] = useState('');
+  const [eventName, setEventName] = useState("");
+  const [description, setEventDesc] = useState("");
+  const [location, setLocation] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [eventType, setEventType] = useState("");
   const [eventSaved, setEventSaved] = useState(false);
   const [error, setError] = useState(false);
+
+  async function createEvent(eventData) {
+    try {
+      const response = await fetch("http://localhost:8888/events/createEvent", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(eventData),
+      });
+      const data = await response.json();
+      console.log("CreateEvent response:", data);
+      if (!response.ok) {
+        throw new Error("Create event failed");
+      }
+      console.log("Event created successfully:", eventData);
+      navigate("/");
+    } catch (error) {
+      console.error("Error creating event:", error);
+      setError(true);
+    }
+  }
 
   const handleSaveEvent = (e) => {
     e.preventDefault();
 
     // Validation of fields
-    if (!eventName || !location || !date || !eventType) {
+    if (
+      !eventName ||
+      !location ||
+      !description ||
+      !startDate ||
+      !endDate ||
+      !eventType
+    ) {
       setError(true);
       return;
     }
 
+    //TODO: Add real image + uid
     // Object with event data
     const eventData = {
       eventName,
+      description,
       location,
-      date,
+      startDate,
+      endDate,
       eventType,
+      image: "",
+      uid: "1",
     };
 
     // Here you can send the data to the backend to save it
-
+    createEvent(eventData);
     // Logic to simulate the event being saved
     setEventSaved(true);
   };
@@ -53,6 +89,15 @@ function NewEvent() {
             <input
               className="input"
               type="text"
+              placeholder="Event Description"
+              value={eventName}
+              onChange={(e) => setEventDesc(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="input"
+              type="text"
               placeholder="Location"
               value={location}
               onChange={(e) => setLocation(e.target.value)}
@@ -62,24 +107,28 @@ function NewEvent() {
             <input
               className="input"
               type="date"
-              placeholder="Date"
-              value={date}
-              onChange={(e) => setDate(e.target.value)}
+              placeholder="Start Date"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
             />
           </div>
           <div className="form-group">
-            <select
+            <input
               className="input"
+              type="date"
+              placeholder="End Date"
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="input"
+              type="text"
+              placeholder="Event type"
               value={eventType}
               onChange={(e) => setEventType(e.target.value)}
-            >
-              <option value="">Event Type</option>
-              <option value="Concert">Concert</option>
-              <option value="Sports">Sports</option>
-              <option value="Theatre">Theatre</option>
-              <option value="Festival">Festival</option>
-              {/* Add more options as needed */}
-            </select>
+            />
           </div>
           <button className="button" type="submit" onClick={handleSaveEvent}>
             Save Event
