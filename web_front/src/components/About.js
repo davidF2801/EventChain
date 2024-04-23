5a8a5b2e1240a5a0093dea8f21f0ee3865081833
dumre00 <gokarnadumre@gmail.com>
2024-04-23 19:17:29 +0200

    resolving pictures problems

--- web_front/src/components/About.js
+++ web_front/src/components/About.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
+
+// Importa las imágenes con la ruta correcta
+import erikImage from "./images/erik.jpeg";
+import joelImage from "./images/joel.jpeg";
+import gokarnaImage from "./images/gokarna.jpeg";
+import davidImage from "./images/david.jpeg";
 
 function About() {
-
   return (
     <div>
-      <h2 style={{ color: 'navy' }}>About Us</h2>
+      <h2 style={{ color: "navy" }}>About Us</h2>
 
       <section>
         <p>Los jefes del blockchain</p>
@@ -12,54 +17,120 @@ function About() {
 
       <section>
         <h3>What we do</h3>
-        <div style={{ maxWidth: '900px', margin: '0 auto' }}> {/* Neuer Container mit begrenzter Breite und horizontal zentriertem Inhalt */}
+        <div style={{ maxWidth: "900px", margin: "0 auto" }}>
           <p>Nos vamos con las samaritanas del amor</p>
-        </div>      </section>
+        </div>
+      </section>
 
       <section>
         <h3>Our team</h3>
-        <div className="estudiantes" style={{ display: 'flex', justifyContent: 'center' }}>
+        <div
+          className="estudiantes"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
           {/* Primera sección con tres personas */}
-          <div style={{ display: 'flex', marginRight: '10px' }}>
-            <div className="estudiante" style={{ marginRight: '10px' }}>
+          <div style={{ display: "flex", marginRight: "10px" }}>
+            <div className="estudiante" style={{ marginRight: "10px" }}>
               <a href="https://www.linkedin.com/in/eriksole/">
-                <img src={require('./images/erik.jpeg')} alt="erik" className="foto-erik" style={{ height: '200px', width: '200px', borderRadius: '50%' }} />
-                <p><strong>Erik Sole</strong></p>
-
+                <img
+                  src={erikImage}
+                  alt="Erik Sole"
+                  className="foto-erik"
+                  style={{
+                    height: "200px",
+                    width: "200px",
+                    borderRadius: "50%",
+                  }}
+                />
+                <p>
+                  <strong>Erik Sole</strong>
+                </p>
               </a>
-              <p> Brazzer<br />Engineer</p>
+              <p>
+                {" "}
+                Brazzer
+                <br />
+                Engineer
+              </p>
             </div>
-            <div className="estudiante" style={{ marginRight: '10px' }}>
+            <div className="estudiante" style={{ marginRight: "10px" }}>
               <a href="https://www.linkedin.com/in/joel-jurado-4a9a8b208/">
-                <img src={require('./images/joel.jpeg')} alt="Joel Jurado" className="foto-joel" style={{ height: '200px', width: '200px', borderRadius: '50%' }} />
-                <p><strong>Joel Jurado</strong></p>
+                <img
+                  src={joelImage}
+                  alt="Joel Jurado"
+                  className="foto-joel"
+                  style={{
+                    height: "200px",
+                    width: "200px",
+                    borderRadius: "50%",
+                  }}
+                />
+                <p>
+                  <strong>Joel Jurado</strong>
+                </p>
               </a>
-              <p> Pornhub<br /> Engineer</p>
+              <p>
+                {" "}
+                Pornhub
+                <br /> Engineer
+              </p>
             </div>
           </div>
         </div>
-        <div className="estudiantes" style={{ display: 'flex', justifyContent: 'center' }}>
+        <div
+          className="estudiantes"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
           {/* Segunda sección con dos personas */}
-          <div style={{ display: 'flex', marginRight: '10px' }}>
-            <div className="estudiante" style={{ marginRight: '10px' }}>
+          <div style={{ display: "flex", marginRight: "10px" }}>
+            <div className="estudiante" style={{ marginRight: "10px" }}>
               <a href="https://www.linkedin.com/in/gdum/">
-                <img src={require('./images/gokarna.jpeg')} alt="Gokarna" className="foto-gokarna" style={{ height: '200px', width: '200px', borderRadius: '50%' }} />
-                <p><strong>Gokarna Dumre</strong></p>
+                <img
+                  src={gokarnaImage}
+                  alt="Gokarna Dumre"
+                  className="foto-gokarna"
+                  style={{
+                    height: "200px",
+                    width: "200px",
+                    borderRadius: "50%",
+                  }}
+                />
+                <p>
+                  <strong>Gokarna Dumre</strong>
+                </p>
               </a>
-              <p> Exotic<br />  Engineer</p>
+              <p>
+                {" "}
+                Exotic
+                <br /> Engineer
+              </p>
             </div>
-            <div className="estudiante" style={{ marginRight: '10px' }}>
+            <div className="estudiante" style={{ marginRight: "10px" }}>
               <a href="https://www.linkedin.com/in/david-forn%C3%B3s/">
-                <img src={require('./images/david.jpeg')} alt="David" className="foto-david" style={{ height: '200px', width: '200px', borderRadius: '50%' }} />
-                <p><strong>David Fornos</strong></p>
+                <img
+                  src={davidImage}
+                  alt="David Fornos"
+                  className="foto-david"
+                  style={{
+                    height: "200px",
+                    width: "200px",
+                    borderRadius: "50%",
+                  }}
+                />
+                <p>
+                  <strong>David Fornos</strong>
+                </p>
               </a>
-              <p>LatinasLover<br /> Engineer</p>
-          
+              <p>
+                LatinasLover
+                <br /> Engineer
+              </p>
             </div>
           </div>
         </div>
       </section>
-       <footer>
+
+      <footer>
         <p>© 2024 Eventchain. Todos los derechos reservados.</p>
       </footer>
     </div>
--- web_front/src/components/Home.js
+++ web_front/src/components/Home.js
@@ -1,8 +1,25 @@
-import React, { useEffect, useRef } from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import './Home.css';
+import React, { useEffect, useRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./Home.css";
+
+// Ajusta la ruta de las imágenes para que coincida con la estructura de directorios
+import hackupcImage from "./images/hackupc.jpg";
+import mclovingTelecofaraImage from "./images/mcloving_telecofara.jpg";
+import mclovingImage from "./images/mcloving.jpg";
+import menorca11Image from "./images/menorca11.jpg";
+import menorca2Image from "./images/menorca2.jpg";
+import menorca3Image from "./images/menorca3.jpg";
+import menorca4Image from "./images/menorca4.jpg";
+import menorca5Image from "./images/menorca5.jpg";
+import menorca6Image from "./images/menorca6.jpg";
+import menorca7Image from "./images/menorca7.jpg";
+import menorca8Image from "./images/menorca8.jpg";
+import menorca9Image from "./images/menorca9.jpg";
+import telecosImage from "./images/telecos.jpg";
+import telecos1Image from "./images/telecos1.jpg";
+import telecos2Image from "./images/telecos2.jpg";
 
 function Home() {
   const sliderRef = useRef(null);
@@ -25,7 +42,7 @@ function Home() {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
-    pauseOnHover: true
+    pauseOnHover: true,
   };
 
   return (
@@ -37,53 +54,52 @@ function Home() {
       <div className="Home-content">
         <Slider ref={sliderRef} {...settings}>
           <div>
-            <img src={require('./images/hackupc.jpg')} alt="HackUPC" />
+            <img src={hackupcImage} alt="HackUPC" />
           </div>
           <div>
-            <img src={require('./images/mcloving_telecofara.jpg')} alt="Mcloving Telecofara" />
+            <img src={mclovingTelecofaraImage} alt="Mcloving Telecofara" />
           </div>
           <div>
-            <img src={require('./images/mcloving.jpg')} alt="Mcloving" />
+            <img src={mclovingImage} alt="Mcloving" />
           </div>
           <div>
-            <img src={require('./images/menorca11.jpg')} alt="Menorca" />
+            <img src={menorca11Image} alt="Menorca" />
           </div>
           <div>
-            <img src={require('./images/menorca2.jpg')} alt="Menorca 2" />
+            <img src={menorca2Image} alt="Menorca 2" />
           </div>
           <div>
-            <img src={require('./images/menorca3.jpg')} alt="Menorca 3" />
+            <img src={menorca3Image} alt="Menorca 3" />
           </div>
           <div>
-            <img src={require('./images/menorca4.jpg')} alt="Menorca 4" />
+            <img src={menorca4Image} alt="Menorca 4" />
           </div>
           <div>
-            <img src={require('./images/menorca5.jpg')} alt="Menorca 5" />
+            <img src={menorca5Image} alt="Menorca 5" />
           </div>
           <div>
-            <img src={require('./images/menorca6.jpg')} alt="Menorca 6" />
+            <img src={menorca6Image} alt="Menorca 6" />
           </div>
           <div>
-            <img src={require('./images/menorca7.jpg')} alt="Menorca 7" />
+            <img src={menorca7Image} alt="Menorca 7" />
           </div>
           <div>
-            <img src={require('./images/menorca8.jpg')} alt="Menorca 8" />
+            <img src={menorca8Image} alt="Menorca 8" />
           </div>
           <div>
-            <img src={require('./images/menorca9.jpg')} alt="Menorca 9" />
+            <img src={menorca9Image} alt="Menorca 9" />
           </div>
           <div>
-            <img src={require('./images/telecos.jpg')} alt="Telecos" />
+            <img src={telecosImage} alt="Telecos" />
           </div>
           <div>
-            <img src={require('./images/telecos1.jpg')} alt="Telecos 1" />
+            <img src={telecos1Image} alt="Telecos 1" />
           </div>
           <div>
-            <img src={require('./images/telecos2.jpg')} alt="Telecos 2" />
+            <img src={telecos2Image} alt="Telecos 2" />
           </div>
         </Slider>
       </div>
-
 
       <footer>
         <p>© 2024 EventChain. All rights reserved.</p>
