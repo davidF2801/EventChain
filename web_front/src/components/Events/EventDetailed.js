3b3e1645403e5d73350c1e939178f2da7e1aa600
dumre00 <gokarnadumre@gmail.com>
2024-05-05 18:58:29 +0200

    Navbar y Eventdetail updated

--- web_front/src/components/Events/EventDetailed.js
+++ web_front/src/components/Events/EventDetailed.js
@@ -46,76 +46,11 @@ const EventDetailed = () => {
       "_blank"
     );
   };
-  const handleBuy = async (eventInfo) => {
-    try {
-      if (!eventInfo.startDate) {
-        const ticketInfo = await resellTicket(
-          privateKey,
-          publicKey,
-          eventInfo.contractAddress,
-          eventInfo.ticketId
-        );
-        const updateResponse = await fetch(
-          "http://localhost:8888/tickets/rebuyTicket",
-          {
-            method: "PUT",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: "Bearer " + isAuthenticated,
-            },
-            body: JSON.stringify({
-              contractAddress: ticketInfo.contractAddress,
-              ticketId: ticketInfo.ticketId,
-              forSale: false,
-              price: ticketInfo.ticketPrice._hex,
-            }),
-          }
-        );
-        const updateResult = await updateResponse.json();
-        if (updateResponse.ok) {
-          console.log("Ticket updated successfully in database:", updateResult);
-        } else {
-          throw new Error(
-            updateResult.error || "Failed to update ticket in the database"
-          );
-        }
-      } else {
-        const ticketInfo = await buyTicket(eventInfo.contractAddress);
-        console.log("Ticket Info:", ticketInfo);
-        const ticketData = {
-          eventName: eventInfo.title,
-          forSale: false,
-          ticketId: ticketInfo.ticketId,
-          price: ticketInfo.ticketPrice._hex,
-          contractAddress: eventInfo.contractAddress,
-        };
-        console.log(isAuthenticated);
-        fetch("http://localhost:8888/tickets/createTicket", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + isAuthenticated,
-          },
-          body: JSON.stringify(ticketData),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            if (data.success) {
-              console.log("Ticket created successfully:", data);
-            } else {
-              throw new Error(data.error || "Failed to create ticket");
-            }
-          })
-          .catch((error) => {
-            console.error(
-              "Error when creating ticket:",
-              error.message || error
-            );
-          });
-      }
-    } catch (error) {
-      console.error("Error buying ticket:", error.message || error);
-    }
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return date.toLocaleDateString(undefined, options);
   };
 
   if (error) {
@@ -155,12 +90,12 @@ const EventDetailed = () => {
         <p
           style={{ marginBottom: "10px", fontWeight: "bold", color: "#ffcc00" }}
         >
-          Start Date: {eventData.startDate}
+          Start Date: {formatDate(eventData.startDate)}
         </p>
         <p
           style={{ marginBottom: "10px", fontWeight: "bold", color: "#ffcc00" }}
         >
-          Finish Date: {eventData.endDate}
+          Finish Date: {formatDate(eventData.endDate)}
         </p>
         <p
           style={{ marginBottom: "10px", fontWeight: "bold", color: "#66ff99" }}
@@ -188,15 +123,6 @@ const EventDetailed = () => {
               }}
             >
               Resale Fee: {eventData.resaleFee}
-            </p>
-            <p
-              style={{
-                marginBottom: "10px",
-                fontWeight: "bold",
-                color: "#cc66ff",
-              }}
-            >
-              Maximum Price Allowed: {eventData.maxPrice}
             </p>
           </>
         )}
--- web_front/src/components/NavBar.js
+++ web_front/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { IoSearch } from "react-icons/io5";
 import useRequireAuth from "../authenticate_utils.js";
@@ -8,11 +8,10 @@ import logo from "./images/logo1.png";
 
 const Navbar = () => {
   const isAuthenticated = useRequireAuth();
-  if (isAuthenticated == null) {
-    return null;
-  }
   const [searchQuery, setSearchQuery] = useState("");
   const [iconColor, setIconColor] = useState("#888"); // Color inicial del ícono
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 1150); // Estado para verificar si la pantalla es pequeña
+  const [showMenu, setShowMenu] = useState(false); // Estado para controlar si se muestra el menú desplegable
   const navigate = useNavigate();
 
   const handleSearchInputChange = (e) => {
@@ -29,77 +28,223 @@ const Navbar = () => {
     }
   };
 
+  // Función para alternar la visibilidad del menú desplegable
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      const isSmall = window.innerWidth < 1150;
+      setIsSmallScreen(isSmall);
+      if (!isSmall) {
+        setShowMenu(false); // Ocultar el menú desplegable cuando la pantalla deja de ser pequeña
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+    <nav
+      className="w-full flex md:justify-center justify-between items-center p-4"
+      style={{ fontFamily: "Orbitron" }}
+    >
       <header className="Header">
-        <div className="Header-left">
-          <Link to="/" className="Header-title">
-            <img src={logo} className="Header-logo" alt="Logo" />
-            EventChain
-          </Link>
-          <nav className="Navbar">
-            <ul className="Navbar-links">
-              <li>
-                <Link to="/events" className="login-link">
-                  All Events
-                </Link>
-              </li>
-              <li>
-                <Link to="/resale" className="login-link">
-                  Resale Marketplace
-                </Link>
-              </li>
-              <li>
-                <Link to="/about" className="login-link">
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link to="/contact" className="login-link">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div className="Header-right">
-          <div className="searchbox-container" style={{ position: "relative" }}>
-            <input
-              type="text"
-              placeholder="Search..."
-              className="searchbox"
-              value={searchQuery}
-              onChange={handleSearchInputChange}
-              onKeyPress={handleKeyPress}
-              style={{ paddingRight: "40px" }}
-            />
-            <IoSearch
-              className="search-icon"
-              style={{
-                position: "absolute",
-                top: "50%",
-                right: "10px",
-                transform: "translateY(-50%)",
-                color: iconColor, // Color dinámico
-                cursor: "pointer",
-              }}
-              onClick={handleSearch}
-              onMouseEnter={() => setIconColor("#007bff")} // Cambiar a azul cuando pasa el cursor
-              onMouseLeave={() => setIconColor("#888")} // Cambiar al color original cuando sale el cursor
-            />
-            {isAuthenticated ? (
-              <>
-                <Link to="/myprofile" className="login-link">
-                  My Profile
-                </Link>
-              </>
-            ) : (
-              <Link to="/login" className="login-link">
-                Login
+        {!isSmallScreen && (
+          <>
+            <div className="Header-left">
+              <Link to="/" className="Header-title">
+                <img src={logo} className="Header-logo" alt="Logo" />
+                EventChain
               </Link>
-            )}
+              <nav className="Navbar">
+                <ul className="Navbar-links">
+                  <li>
+                    <Link
+                      to="/events"
+                      className="login-link"
+                      style={{ fontFamily: "Orbitron" }}
+                    >
+                      All Events
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/resale"
+                      className="login-link"
+                      style={{ fontFamily: "Orbitron" }}
+                    >
+                      Resale Marketplace
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/about"
+                      className="login-link"
+                      style={{ fontFamily: "Orbitron" }}
+                    >
+                      About
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/contact"
+                      className="login-link"
+                      style={{ fontFamily: "Orbitron" }}
+                    >
+                      Contact
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div
+              className="Header-right"
+              style={{ display: "flex", alignItems: "center" }}
+            >
+              <div
+                className="searchbox-container"
+                style={{ position: "relative", fontFamily: "Orbitron" }}
+              >
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  className="searchbox"
+                  value={searchQuery}
+                  onChange={handleSearchInputChange}
+                  onKeyPress={handleKeyPress}
+                  style={{ paddingRight: "40px" }}
+                />
+                <IoSearch
+                  className="search-icon"
+                  style={{
+                    position: "absolute",
+                    top: "50%",
+                    right: "10px",
+                    transform: "translateY(-50%)",
+                    color: iconColor,
+                    cursor: "pointer",
+                  }}
+                  onClick={handleSearch}
+                  onMouseEnter={() => setIconColor("#007bff")}
+                  onMouseLeave={() => setIconColor("#888")}
+                />
+              </div>
+              <div style={{ marginLeft: "20px" }}>
+                {isAuthenticated ? (
+                  <Link
+                    to="/myprofile"
+                    className="login-link"
+                    style={{ fontFamily: "Orbitron" }}
+                  >
+                    My Profile
+                  </Link>
+                ) : (
+                  <Link
+                    to="/login"
+                    className="login-link"
+                    style={{ fontFamily: "Orbitron" }}
+                  >
+                    Login
+                  </Link>
+                )}
+              </div>
+            </div>
+          </>
+        )}
+        {isSmallScreen && (
+          <div className="Header-left">
+            <Link to="/" className="Header-title">
+              <img src={logo} className="Header-logo" alt="Logo" />
+              EventChain
+            </Link>
+            <div className="Header-right">
+              <button
+                className="toggle-menu-btn"
+                onClick={toggleMenu}
+                style={{
+                  background: "linear-gradient(to left, #330cc0, #9547d4)",
+                  color: "#fff",
+                  position: "fixed",
+                  top: "5px", // Margen superior
+                  right: "20px", // Margen derecho
+                  zIndex: "990", // Asegura que el botón esté por encima de otros elementos
+                  padding: "10px", // Espacio alrededor del texto dentro del botón
+                  borderRadius: "50%", // Botón redondeado
+                  border: "none", // Sin borde
+                }}
+              >
+                ☰
+              </button>
+              {showMenu && (
+                <nav className="menu-sidebar">
+                  <ul>
+                    <li>
+                      <Link
+                        to="/events"
+                        className="login-link"
+                        style={{ fontFamily: "Orbitron" }}
+                      >
+                        All Events
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/resale"
+                        className="login-link"
+                        style={{ fontFamily: "Orbitron" }}
+                      >
+                        Resale Marketplace
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/about"
+                        className="login-link"
+                        style={{ fontFamily: "Orbitron" }}
+                      >
+                        About
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/contact"
+                        className="login-link"
+                        style={{ fontFamily: "Orbitron" }}
+                      >
+                        Contact
+                      </Link>
+                    </li>
+                    <li>
+                      {isAuthenticated ? (
+                        <Link
+                          to="/myprofile"
+                          className="login-link"
+                          style={{ fontFamily: "Orbitron" }}
+                        >
+                          My Profile
+                        </Link>
+                      ) : (
+                        <Link
+                          to="/login"
+                          className="login-link"
+                          style={{ fontFamily: "Orbitron" }}
+                        >
+                          Login
+                        </Link>
+                      )}
+                    </li>
+                  </ul>
+                </nav>
+              )}
+            </div>
           </div>
-        </div>
+        )}
       </header>
     </nav>
   );
