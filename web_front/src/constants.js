5df085dd9b33750b43df890f71701023f489efa6
Your Name <you@example.com>
2024-05-07 20:09:54 +0200

    fix

--- web_front/src/constants.js
+++ web_front/src/constants.js
@@ -1,3 +1,3 @@
-// const SERVER_ADDRESS = "https://13.48.5.247:443";
-const SERVER_ADDRESS = "http://localhost:8888";
-export { SERVER_ADDRESS };
+const SERVER_ADDRESS = "https://13.48.5.247:443";
+// const SERVER_ADDRESS = "http://localhost:8888";
+// export { SERVER_ADDRESS };
