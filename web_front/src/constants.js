97dc026c1a1fbe74d6526683a2d3b3f220888d9c
davidF2801 <dfornosf@gmail.com>
2024-06-26 20:43:09 +0200

    Payment for posting event

--- web_front/src/constants.js
+++ web_front/src/constants.js
@@ -1,3 +1,5 @@
 const SERVER_ADDRESS = "https://13.48.5.247:443";
 //const SERVER_ADDRESS = "http://localhost:8888";
-export { SERVER_ADDRESS };
+const TRON_ADDRESS = "TAEXBVR2pBxYpFDEdsF3UXws7zMzZTfy9T";
+const EVENT_FEE = 500 * 1000000;
+export { SERVER_ADDRESS, TRON_ADDRESS, EVENT_FEE };
